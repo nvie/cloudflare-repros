b5f94098bba39681d98a62f5ab916e13ba346847
Vincent Driessen <me@nvie.com>
2024-04-16 15:25:14 +0200

    Expend server a bit to report about socket states

--- durable-objects/src/index.ts
+++ durable-objects/src/index.ts
@@ -1,5 +1,33 @@
+function formatReadyState(ws: WebSocket) {
+  switch (ws.readyState) {
+    case WebSocket.CONNECTING:
+      return "CONNECTING";
+    case WebSocket.OPEN:
+      return "OPEN";
+    case WebSocket.CLOSING:
+      return "CLOSING";
+    case WebSocket.CLOSED:
+      return "CLOSED";
+    default:
+      throw new Error("Unknown ready state");
+  }
+}
+
 export class Demo implements DurableObject {
-  constructor(readonly state: DurableObjectState) {}
+  constructor(readonly state: DurableObjectState) {
+    console.log("Running constructor");
+    this.printSockets();
+
+    let index = 0;
+    for (const ws of this.state.getWebSockets()) {
+      index++;
+      try {
+        ws.send("beep");
+      } catch (err) {
+        console.error(`Could not send a beep to socket#${index}: ${err}`);
+      }
+    }
+  }
 
   fetch(req: Request) {
     const pair = new WebSocketPair();
@@ -13,11 +41,23 @@ export class Demo implements DurableObject {
     }
 
     this.state.acceptWebSocket(server);
+    this.printSockets();
 
     return new Response(null, { status: 101, webSocket: client });
   }
 
+  printSockets() {
+    console.log("--------------------------------------------------");
+    let index = 0;
+    for (const ws of this.state.getWebSockets()) {
+      index++;
+      console.log(`- Socket#${index} state=${formatReadyState(ws)}`);
+    }
+  }
+
   async webSocketMessage(ws: WebSocket, msg: string | ArrayBuffer) {
+    console.log(`Server received ${msg}`);
+
     switch (msg) {
       case "now?":
         ws.send(`now=${Date.now()}`);
@@ -41,13 +81,13 @@ export class Demo implements DurableObject {
     }
   }
 
-  // async webSocketClose(ws: WebSocket) {
-  //   console.log(`Got CLOSE event for socket ${ws}`);
-  // }
+  async webSocketClose(ws: WebSocket) {
+    console.log(`Got CLOSE event for socket ${ws}`);
+  }
 
-  // async webSocketError(ws: WebSocket) {
-  //   console.log(`Got ERROR event for socket ${ws}`);
-  // }
+  async webSocketError(ws: WebSocket) {
+    console.log(`Got ERROR event for socket ${ws}`);
+  }
 }
 
 export default <ExportedHandler<Env>>{
